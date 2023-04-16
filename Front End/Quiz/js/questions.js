6b50d65b5fdbce0f30c82143bdc02d2c2c9a8f8b
gurubazawada <110350867+gurubazawada@users.noreply.github.com>
2023-04-16 13:56:03 -0500

    post presentation

--- Front End/Quiz/js/questions.js
+++ Front End/Quiz/js/questions.js
@@ -1,72 +1,58 @@
 // creating an array and passing the number, questions, options, and answers
 let questions = [
-    {
+  {
     numb: 1,
-    question: "What does HTML stand for?",
-    answer: "Hyper Text Markup Language",
+    question: "What types of data are associated with NBA player heights?",
+    answer: "Continuous numerical data and nominal/ordinal categorical data",
     options: [
-      "Hyper Text Preprocessor",
-      "Hyper Text Markup Language",
-      "Hyper Text Multiple Language",
-      "Hyper Tool Multi Language"
+      "Discrete numerical data and ordinal categorical data",
+      "Continuous numerical data and nominal/ordinal categorical data",
+      "Discrete numerical data and nominal categorical data",
+      "Continuous numerical data and ordinal categorical data"
     ]
   },
-    {
+  {
     numb: 2,
-    question: "What does CSS stand for?",
-    answer: "Cascading Style Sheet",
+    question: "What are proportions and how are they expressed?",
+    answer: "They are expressed as a percentage out of 100 and are made up of numerical summary figures once aggregated",
     options: [
-      "Common Style Sheet",
-      "Colorful Style Sheet",
-      "Computer Style Sheet",
-      "Cascading Style Sheet"
+      "They are expressed as a ratio and are made up of numerical summary figures once aggregated",
+      "They are expressed as a percentage out of 100 and are made up of categorical summary figures once aggregated",
+      "They are expressed as a ratio and are made up of categorical summary figures once aggregated",
+      "They are expressed as a percentage out of 100 and are made up of numerical summary figures once disaggregated"
     ]
   },
-    {
+  {
     numb: 3,
-    question: "What does PHP stand for?",
-    answer: "Hypertext Preprocessor",
+    question: "What is the probability density function?",
+    answer: "It describes the distribution of all the players in between the tallest and the shortest players in relation to their frequency of occurring",
     options: [
-      "Hypertext Preprocessor",
-      "Hypertext Programming",
-      "Hypertext Preprogramming",
-      "Hometext Preprocessor"
+      "It describes the distribution of all the players in relation to their height only",
+      "It describes the distribution of all the players in relation to their weight only",
+      "It describes the distribution of all the players in relation to their age only",
+      "It describes the distribution of all the players in between the tallest and the shortest players in relation to their frequency of occurring"
     ]
   },
-    {
+  {
     numb: 4,
-    question: "What does SQL stand for?",
-    answer: "Structured Query Language",
+    question: "What is p-hacking?",
+    answer: "A dangerous practice in research where a researcher collects data, tests many different effects, and finds the one with a p-value less than 0.05 in order to pretend that this was the result they were looking for the whole time",
     options: [
-      "Stylish Question Language",
-      "Stylesheet Query Language",
-      "Statement Question Language",
-      "Structured Query Language"
+      "A harmless practice in research where a researcher collects data, tests many different effects, and finds the one with a p-value less than 0.05 in order to confirm their hypothesis",
+      "A dangerous practice in research where a researcher collects data, tests only one effect, and finds the one with a p-value less than 0.05 in order to confirm their hypothesis",
+      "A harmless practice in research where a researcher collects data, tests only one effect, and finds the one with a p-value greater than 0.05 in order to reject their hypothesis",
+      "A dangerous practice in research where a researcher collects data, tests many different effects, and finds the one with a p-value greater than 0.05 in order to reject their hypothesis"
     ]
   },
-    {
+  {
     numb: 5,
-    question: "What does XML stand for?",
-    answer: "eXtensible Markup Language",
+    question: "What is the limitation of sample statistic due to small sample size?",
+    answer: "It may not be representative of the population and confidence intervals are necessary to assess whether the sample is reliable",
     options: [
-      "eXtensible Markup Language",
-      "eXecutable Multiple Language",
-      "eXTra Multi-Program Language",
-      "eXamine Multiple Language"
+      "It may not be representative of the population and hypothesis tests are necessary to assess whether the sample is reliable",
+      "It may be representative of the population but confidence intervals are still necessary to assess whether the sample is reliable",
+      "It may not be representative of the population but hypothesis tests are still necessary to assess whether the sample is reliable",
+      "It may be representative of the population and hypothesis tests are still necessary to assess whether the sample is reliable"
     ]
-  },
-  // you can uncomment the below codes and make duplicate as more as you want to add question
-  // but remember you need to give the numb value serialize like 1,2,3,5,6,7,8,9.....
-
-  //   {
-  //   numb: 6,
-  //   question: "Your Question is Here",
-  //   answer: "Correct answer of the question is here",
-  //   options: [
-  //     "Option 1",
-  //     "option 2",
-  //     "option 3",
-  //     "option 4"
-  //   ]
-  // },
+  }
 ];
